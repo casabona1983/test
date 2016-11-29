a80d4816d404dfca5e409c8151f4ed4c1fac7bd7
Casabona <casabona1983@gmail.com>
2016-11-29 23:51:46 +0100

    casa  20161129 2351

--- gulpfile.js
+++ gulpfile.js
@@ -3,51 +3,45 @@ var compass = require('gulp-compass');
 var cssnano = require('gulp-cssnano');
 var autoprefixer = require('gulp-autoprefixer');
 var inject = require('gulp-inject');
-var sass = require('gulp-sass');
+
+var
+    source = 'src/',
+    dest = 'dist/';
+
+var bootstrapSass = { in: './node_modules/bootstrap-sass/'
+};
+
+var fonts = { in: [source + 'fonts/*.*', bootstrapSass.in + 'assets/fonts/**/*'],
+    out: dest + 'fonts/'
+};
+
+gulp.task('fonts', function () {
+    return gulp
+        .src(fonts.in)
+        .pipe(gulp.dest(fonts.out));
+});
 
 
-gulp.task('compass', function () {
-
-    var injectAppFiles = gulp.src('src/styles/*.scss', {
-        read: false
-    });
-
-    function transformFilepath(filepath) {
-        return '@import "' + filepath + '";';
-    }
-
-    var injectAppOptions = {
-        transform: transformFilepath,
-        starttag: '// inject:app',
-        endtag: '// endinject',
-        addRootSlash: false
-    };
-    /*return gulp.src('src/main.scss')
-        .pipe(inject(injectAppFiles, injectAppOptions))
+gulp.task('compass', ['fonts'], function () {
+    return gulp.src([source + 'main.scss'])
         .pipe(compass({
-            css: 'dist/styles',
-            sass: 'src',
-            image: 'dist/images'
+            sass: source,
+            import_path:bootstrapSass.in + 'assets/stylesheets'
         }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(cssnano())
-        .pipe(sass())
-        .pipe(gulp.dest('dist/styles'));*/
-    return gulp.src('src/main.scss')
-        .pipe(inject(injectAppFiles, injectAppOptions))
-        .pipe(sass())
-        .pipe(gulp.dest('dist/styles'));
+        .pipe(gulp.dest(dest + 'styles'));
 });
 
 gulp.task('traspaso-build', ['compass'], function () {
-    var injectFiles = gulp.src(['dist/styles/main.css']);
+    var injectFiles = gulp.src([dest + 'styles/main.css']);
     var injectOptions = {
         addRootSlash: false,
-        ignorePath: ['src', 'dist']
+        ignorePath: [source, dest]
     };
-    return gulp.src(['src/index.html'])
+    return gulp.src([source + 'index.html'])
         .pipe(inject(injectFiles, injectOptions))
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest(dest));
 });
 
 gulp.task('default', ['traspaso-build'], function () {});
